e5009b34c1f4ae009d698b0019c30db686b6f52d
alekandardonev <alekandardonev@users.noreply.github.com>
2024-07-06 00:00:00 +0000

    Add Python tests

---
+++ tools/svc_test.py
@@ -0,0 +1,39 @@
+import boto3
+import botocore
+from botocore.exceptions import EndpointConnectionError
+
+res = 'sqs'
+svc = 'get_queue_attributes'
+params = {'QueueUrl': "https://sqs.eu-west-3.amazonaws.com/559583391002/ma-queue-mickey", "AttributeNames": ["All"]}
+region = 'eu-west-3'
+#region = None
+#region = 'us-east-1'
+#region = 'global'
+
+def print_exception(order, e):
+    print('-'*72)
+    print(order, type(e), e)
+    print('-'*72) 
+
+try:
+    # Get the service resource
+    client = boto3.client(res, region_name=region)
+
+    # Create the queue. This returns an SQS.Queue instance
+    res = client.__getattribute__(svc)(**params)
+    res.pop('ResponseMetadata', None)
+
+    # You can now access identifiers and attributes
+    print(res)
+
+#except AWSOrganizationsNotInUseException as e1:
+#    print_exception(1, e1)
+except AttributeError as e2:
+    print_exception(2, e2)
+except botocore.exceptions.ClientError as e3:
+    print_exception(3, e3)
+except EndpointConnectionError as e4:
+    print_exception(4, e4)
+except Exception as e:
+    print_exception(5, e)
+
